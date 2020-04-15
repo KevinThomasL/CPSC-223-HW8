bad0b0a0e59ce9d3b09d1cb0b2b197da7b216f3c
CoderKevL <Kevin.Lunden@gmail.com>
2020-04-15 15:20:53 -0700

    Everything working and tests added

--- hash_table_collection.h
+++ hash_table_collection.h
@@ -1,10 +1,9 @@
 
 //----------------------------------------------------------------------
-// Author: ...
+// Author: Kevin Lunden
 // Course: CPSC 223, Spring 2020
 // Assign: 8
 // File:   hash_table_collection.h
-// ...
 //----------------------------------------------------------------------
 
  
@@ -84,8 +83,329 @@ private:
    
 };
 
-
 // TODO: implement the above functions here ...
+// create an empty linked
+template <typename K, typename V>
+HashTableCollection <K,V>::HashTableCollection() : collection_size(0), table_capacity(16), load_factor_threshold(0.75) 
+{
+ // dynamically allocate the hash table array
+ hash_table = new Node*[table_capacity];
 
+ for (int i = 0; i < table_capacity; ++i)
+  hash_table[i] = nullptr;
+}
+
+// copy a linked list
+template <typename K, typename V>
+HashTableCollection<K,V>::HashTableCollection(const HashTableCollection<K,V>& rhs) : hash_table(nullptr)
+{
+ *this = rhs;
+}
+
+// assign a linked list
+template <typename K, typename V>
+HashTableCollection <K,V>& HashTableCollection <K,V>::operator=(const HashTableCollection<K,V>& rhs)
+{
+ // check if rhs is current object and return current object
+ if (this == &rhs)
+  return *this;
+
+ make_empty ();
+
+ // initialize current object
+ collection_size = 0;
+ load_factor_threshold = 0.75;
+  
+ hash_table = new Node*[rhs.table_capacity];
+
+ //copy
+ Node* ptr;
+ for(int i = 0; i < rhs.table_capacity; i++)
+ {
+  ptr = rhs.hash_table[i];
+  while(ptr != nullptr)
+  {
+   insert(ptr->key, ptr->value);
+   ptr = ptr->next;
+  }
+ }
+ return *this;
+}
+
+// delete a linked list
+template <typename K, typename V>
+HashTableCollection <K,V>::~HashTableCollection()
+{
+  make_empty();
+}
+
+// insert a key-value pair into the collection
+template <typename K, typename V>
+void HashTableCollection<K,V>::add(const K& key, const V& val)
+{
+ // check current load factor 
+ std::hash<K> hash_fun;
+ size_t value = hash_fun(key);
+ size_t index = value % table_capacity;
+
+ Node* ptr = new Node;
+ ptr->key = key;
+ ptr->value = val;
+
+ //resize if necessary
+ if(double(collection_size / table_capacity) > load_factor_threshold)
+ {
+  resize_and_rehash();
+ }
+
+ if(hash_table[index] == nullptr)
+ {
+  ptr->next = nullptr;
+
+  //set the bucket pointer to the head of new node
+  hash_table[index] = ptr;
+ } 
+ else if (hash_table[index] != nullptr)
+ {
+  ptr->next = hash_table[index];
+
+  //bucket now points to head of new node
+  hash_table[index] = ptr;
+ }
+
+ collection_size++;
+}
+
+// remove a key-value pair from the collection
+template <typename K, typename V>
+void HashTableCollection<K,V>::remove(const K& key)
+{
+ std::hash<K> hash_fun;
+ size_t value = hash_fun(key);
+ size_t index = value % table_capacity;
+ Node* ptr;
+ Node* temp;
+
+ //make sure hash table isn't empty
+ if(hash_table != nullptr)
+ {
+  ptr = hash_table[index];
+  if(hash_table[index] != nullptr)
+  {
+   //if the first node has the key
+   if(ptr->key == key)
+   { 
+    //if there is only one node  
+    if(ptr->next == nullptr)
+    {
+     delete ptr;
+     hash_table[index] = nullptr;
+     collection_size--;
+    } 
+    else 
+    {
+     hash_table[index] = ptr->next;
+     ptr->next = nullptr;
+     delete ptr;
+     collection_size--;
+    }
+   }
+   else
+   {
+    temp = ptr;
+    if(ptr != nullptr)
+    {
+     ptr = ptr -> next;
+
+     //while not at the last node of the bucket
+     while(ptr != nullptr)
+     {
+      if(ptr->key == key)
+      {
+       if(ptr->next = nullptr)
+       {
+        temp->next = nullptr;
+        delete ptr;
+        collection_size--;
+       } 
+       else
+       {
+        temp->next = ptr->next;
+        ptr->next = nullptr;
+        delete ptr;
+        collection_size--;
+       }
+      }
+      temp = ptr;
+      ptr = ptr->next;
+     }
+    }
+   }
+  }
+ }
+}
+
+// find the value associated with the key
+template <typename K, typename V>
+bool HashTableCollection<K,V>::find(const K& key, V& val) const
+{
+ Node* temp;
+ std::hash<K> hash_fun;
+ size_t value = hash_fun(key);
+ size_t index = value % table_capacity;
+
+ if(hash_table != nullptr)
+ {
+  if(hash_table[index] == nullptr)
+  {
+   return false;
+  } 
+  else 
+  {
+   temp = hash_table[index];
+
+   while(temp != nullptr)
+   {
+    if(temp->key == key)
+    {
+     val = temp -> value;
+     return true;
+    } 
+    else
+    {
+     temp = temp->next;
+    }
+   }
+  }
+ }
+ return false;
+}
+
+// find and return the values with keys >= to k1 and <= to k2
+template <typename K, typename V>
+void HashTableCollection<K,V>::find(const K& k1, const K& k2, std::vector<V>& vals) const
+{
+ Node* ptr;
+
+ for(int i = 0; i < table_capacity; i++)
+ {
+  ptr = hash_table[i];
+
+  while(ptr != nullptr)
+  {
+   if(ptr->key >= k1 && ptr->key <= k2)
+   {
+    vals.push_back(ptr->value);
+   }
+
+   ptr = ptr->next;
+  }
+ }
+}
+
+// return all keys in the collection
+template <typename K, typename V>
+void HashTableCollection<K,V>::keys(std::vector <K>& keys) const
+{
+ Node* ptr;
+
+ for(int i = 0; i < table_capacity; i++)
+ {
+  ptr = hash_table[i];
+
+  while(ptr != nullptr)
+  {
+   keys.push_back(ptr->key);
+   ptr = ptr->next;
+  }
+ }
+}
+
+// return collection keys in sorted order
+template<typename K,typename V>
+void HashTableCollection<K,V>::sort(std::vector<K>& ks) const
+{
+ keys(ks);
+ std::sort(ks.begin(), ks.end());
+}
+
+// return the number of keys in collection
+template <typename K, typename V>
+int HashTableCollection <K,V>::size() const
+{
+ return collection_size;
+}
+
+// helper to empty entire hash table
+template <typename K, typename V>
+void HashTableCollection <K,V>::make_empty()
+{
+ Node* ptr;
+ Node* next;
+
+ if(hash_table != nullptr)
+ {
+  for(int i = 0; i < collection_size; i++)
+  {
+   ptr = hash_table[i];
+
+   //iterate through entire linked list in spot of hash table_capacity
+   while(hash_table[i] != nullptr)
+   {
+    next = ptr->next;
+    delete ptr;
+    ptr = next;
+    hash_table[i] = ptr;
+   }
+  }
+ }
+
+ delete hash_table;
+}
+
+template <typename K, typename V>
+void HashTableCollection<K,V>::resize_and_rehash()
+{
+ int new_capacity = table_capacity * 2;
+
+ Node** new_table = new Node*[new_capacity];
+ 
+ // initialize new table
+ for(int i = 0; i < new_capacity; i++)
+  new_table[i] = nullptr;
+
+ // insert key values
+ std::vector <K> ks;
+ keys(ks);
+ size_t index;
+ V inVal;
+
+ for(K key:ks)
+ {
+  std::hash<K> hash_fun;
+  size_t value = hash_fun(key);
+  size_t index = value % table_capacity;
+
+  Node* newNode = new Node;
+  bool temp = find(key, inVal);
+  newNode->key = key;
+  newNode->value = inVal;
+
+  if(new_table[index] == nullptr)
+  {
+   hash_table[index] = newNode;
+   newNode->next = nullptr;
+  } 
+  else 
+  {
+   newNode->next = new_table[index];
+   new_table[index] = newNode;
+  }
+ }
+
+ make_empty();
+ hash_table = new_table;
+ table_capacity = new_capacity;
+}
 
 #endif
--- hw8_test.cpp
+++ hw8_test.cpp
@@ -4,7 +4,7 @@
 // Assign: 8
 // File:   hw8_test.cpp
 //
-// TODO: Description here
+// TODO: Tests functions of hash table collection.
 //----------------------------------------------------------------------
 
 
@@ -14,7 +14,6 @@
 #include "hash_table_collection.h"
 
 using namespace std;
-
 
 // Test 1
 TEST(BasicListTest, CorrectSize) {
@@ -26,10 +25,26 @@ TEST(BasicListTest, CorrectSize) {
   ASSERT_EQ(2, c.size());
   c.add("c", 20.0);
   ASSERT_EQ(3, c.size());
-
 }
 
 // Test 2
+// Tests multiple adds with negative and large numbers
+// Also tests adding same keys
+TEST(BasicListTest, DiffCorrectSize) {
+  HashTableCollection<string,double> c;
+  ASSERT_EQ(0, c.size());
+  c.add("b", 10.0);
+  ASSERT_EQ(1, c.size());
+  c.add("a", 20.0);
+  ASSERT_EQ(2, c.size());
+  c.add("c", 20.0);
+  c.add("d", 1000.0);
+  c.add("d", -10.0);
+  ASSERT_EQ(5, c.size());
+}
+
+
+// Test 3
 TEST(BasicListTest, SimpleFind) {
   HashTableCollection<string,double> c;
   double v;
@@ -43,7 +58,7 @@ TEST(BasicListTest, SimpleFind) {
   ASSERT_EQ(20.0, v);
 }
 
-// Test 3
+// Test 4
 TEST(BasicListTest, SimpleRemoveElems) {
   HashTableCollection<string,int> c;
   c.add("b", 10);
@@ -66,7 +81,7 @@ TEST(BasicListTest, SimpleRemoveElems) {
   ASSERT_EQ(false, c.find("c", v));  
 }
 
-// Test 4
+// Test 5
 TEST(BasicListTest, SimpleRange) {
   HashTableCollection<int,string> c;
   c.add(50, "e");
@@ -87,7 +102,29 @@ TEST(BasicListTest, SimpleRange) {
   ASSERT_EQ(vs.end(), find(vs.begin(), vs.end(), "f"));  
 }
 
-// Test 5
+// Test 6
+// Tests if entire range works
+TEST(BasicListTest, DiffRange) {
+  HashTableCollection<int,string> c;
+  c.add(50, "e");
+  c.add(10, "a");
+  c.add(30, "c");
+  c.add(40, "d");
+  c.add(60, "f");
+  c.add(20, "b");
+  vector<string> vs;
+  c.find(10, 60, vs);
+  ASSERT_EQ(6, vs.size());
+  // note that the following "find" is a C++ built-in function
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "a"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "b"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "c"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "d"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "e"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "f"));
+}
+
+// Test 7
 TEST(BasicListTest, SimpleSort) {
   HashTableCollection<string,int> c;
   c.add("a", 10);
@@ -103,8 +140,38 @@ TEST(BasicListTest, SimpleSort) {
     ASSERT_LE(sorted_ks[i], sorted_ks[i+1]);
 }
 
+// Test 8
+// Test with negative and large numbers
+TEST(BasicListTest, DiffSort) {
+  HashTableCollection<string,int> c;
+  c.add("a", -10);
+  c.add("e", 5000);
+  c.add("c", 30);
+  c.add("b", 20);
+  c.add("d", 40);
+  vector<string> sorted_ks;
+  c.sort(sorted_ks);
+  ASSERT_EQ(5, sorted_ks.size());
+  // check if in sorted order
+  for (int i = 0; i < int(sorted_ks.size()) -1; ++i)
+    ASSERT_LE(sorted_ks[i], sorted_ks[i+1]);
+}
 
-// TODO: ... additional tests ...
+// Test 9
+// Test with multi-letter strings
+TEST(BasicCollectionTest, SimpleEqual){
+  HashTableCollection <string, double> c;
+  c.add("de", 40.0); //make sure remove works on 2 char strings
+  c.add("t", 67.0);
+  double v;
+  c.remove("de");
+  ASSERT_EQ(c.find("de", v), false);
+  c.remove("i"); //make sure removing a string that doesnt exist works
+  ASSERT_EQ(c.find("t", v), true);
+  c.remove("t");
+  c.remove("f");
+  ASSERT_EQ(c.size(), 0); //make sure remove works when empty
+}
 
 int main(int argc, char** argv)
 {
